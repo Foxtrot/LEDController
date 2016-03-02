537a941655bd26e5f2072996a6d4b7af24af8cd0
Foxtrot <foxtrotnull@gmail.com>
2016-03-02 14:44:03 +0000

    Added ability to control the red and blue LEDs.

--- js/module.js
+++ js/module.js
@@ -50,3 +50,87 @@ registerController('TetraYellow', ['$api', '$scope', function($api, $scope) {
 
 	$scope.getTetraYellow();
 }]);
+
+registerController('TetraBlue', ['$api', '$scope', function($api, $scope) {
+	$scope.enabled = false;
+	$scope.trigger = '';
+	$scope.mode = '';
+	$scope.delayOn = '';
+	$scope.delayOff = '';
+	$scope.interface = '';
+
+	$scope.getTetraBlue = (function() {
+		$api.request({
+			module: 'LEDController',
+			action: 'getTetraBlue'
+		}, function(response) {
+			$scope.enabled = response.enabled;
+			$scope.trigger = response.trigger;
+			$scope.mode = response.mode;
+			$scope.delayOn = response.delayOn;
+			$scope.delayOff = response.delayOff;
+			$scope.interface = response.interface;
+			console.log(response);
+		});
+	});
+
+	$scope.setTetraBlue = (function() {
+		$api.request({
+			module: 'LEDController',
+			action: 'setTetraBlue',
+			enabled: $scope.enabled,
+			trigger: $scope.trigger,
+			mode: $scope.mode,
+			delayOn: $scope.delayOn,
+			delayOff: $scope.delayOff,
+			interface: $scope.interface
+		}, function(response) {
+			$scope.getTetraBlue();
+			console.log(response);
+		});
+	});
+
+	$scope.getTetraBlue();
+}]);
+
+registerController('TetraRed', ['$api', '$scope', function($api, $scope) {
+	$scope.enabled = false;
+	$scope.trigger = '';
+	$scope.mode = '';
+	$scope.delayOn = '';
+	$scope.delayOff = '';
+	$scope.interface = '';
+
+	$scope.getTetraRed = (function() {
+		$api.request({
+			module: 'LEDController',
+			action: 'getTetraRed'
+		}, function(response) {
+			$scope.enabled = response.enabled;
+			$scope.trigger = response.trigger;
+			$scope.mode = response.mode;
+			$scope.delayOn = response.delayOn;
+			$scope.delayOff = response.delayOff;
+			$scope.interface = response.interface;
+			console.log(response);
+		});
+	});
+
+	$scope.setTetraRed = (function() {
+		$api.request({
+			module: 'LEDController',
+			action: 'setTetraRed',
+			enabled: $scope.enabled,
+			trigger: $scope.trigger,
+			mode: $scope.mode,
+			delayOn: $scope.delayOn,
+			delayOff: $scope.delayOff,
+			interface: $scope.interface
+		}, function(response) {
+			$scope.getTetraRed();
+			console.log(response);
+		});
+	});
+
+	$scope.getTetraRed();
+}]);
